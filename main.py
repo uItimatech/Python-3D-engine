3535e411bf89ec455d66a25fe51d7fa292679c3d
uItimatech <60111626+uItimatech@users.noreply.github.com>
2022-03-19 23:00:16 +0100

    Update main.py
    -Major optimizations
    -FPS counter
    -Multiple elements support

--- main.py
+++ main.py
@@ -14,11 +14,16 @@ Version: Beta 0.1.3
 
 # ------------ Import libraries --------------
 
+from time import time, sleep 
+from os import system
+from win32api import EnumDisplayDevices, EnumDisplaySettings
+
 from random import randint
 from math import *
-from time import sleep
-from os import system
 
+import numpy as np
+
+# Downloads the missing required modules 
 try:
     from graphics import *
 except ImportError:
@@ -33,16 +38,40 @@ except ImportError:
 
 # ----------- Initialize variables -----------
 
-player_x = float()
-player_y = float()
-player_z = float()
-player_dir_x = float()
-player_dir_y = float()
+class player:
 
-FOV = 90
-VFOV = 45
-window_width = 1080
-window_height = 1080
+    x = float
+    y = float
+    z = float
+
+    class dir:
+        vertical = float
+        horizontal = float
+
+class window:
+    width = 1024
+    height = 1024
+    vertical_resolution = 256
+    horizontal_resolution = 256
+
+class FOV:
+    horizontal = 90
+    vertical = 45
+
+start_clock_time = time.time()
+last_clock_time = 0
+
+class FPS:
+    value = float()
+    lastValue = float()
+    maxValue = 'auto'      # Leave "none" for benchmarking
+    timer = 0
+    counter = object
+
+    if maxValue == 0 or maxValue == 'none':
+        maxValue = 999
+    elif maxValue == 'auto':
+        maxValue = getattr(EnumDisplaySettings(EnumDisplayDevices().DeviceName, -1), 'DisplayFrequency')
 
 renderDebug = dict({
     "wireframe": True,
@@ -51,27 +80,27 @@ renderDebug = dict({
     "orthographic": True
 })
 
-colors = [randint(0,255) for i in range(12)]
 
-window = GraphWin('Render', window_width, window_height, autoflush=False)
-window.setCoords(-window_width, -window_height, window_width, window_height)
+#color_index = [randint(0,255) for i in range(12)]
+color_index = [(255, 0, 0), (255, 0, 0), (0, 255, 0), (0, 255, 0), (0, 0, 255), (0, 0, 255), (128, 128, 0), (128, 128, 0), (128, 0, 128), (128, 0, 128), (0, 128, 128), (0, 128, 128)]
+
+
+
+window = GraphWin('Render', window.width, window.height, autoflush=False)
+window.setCoords(-window.width, -window.height, window.width, window.height)
 #window.master.geometry('%dx%d+%d+%d' % (0, 0, 0, 0))
 
 
 def resetCoords():
 
-    global player_x
-    global player_y
-    global player_z
-    global player_dir_x
-    global player_dir_y
+    global player
 
-    player_x = -105
-    player_y = 65
-    player_z = -15
+    player.x = -105
+    player.y = 65
+    player.z = -15
 
-    player_dir_x = 0
-    player_dir_y = 0
+    player.dir.vertical = 0
+    player.dir.horizontal = 0
 
 
 def rgb(red, green, blue):
@@ -79,49 +108,42 @@ def rgb(red, green, blue):
     return hexValue
 
 
-def drawCube():  # Add x, y, z and rotation parameters
+def drawCube(originX, originY, originZ, Xsize, Ysize, Zsize, *rotation):  # Add x, y, z and rotation parameters
 
     pointArray = []
 
-    pointArray.append(getPoint(50, 0, 50)[0])
-    pointArray.append(getPoint(50, 50, 50)[0])
-    pointArray.append(getPoint(50, 0, 0)[0])
-    pointArray.append(getPoint(50, 50, 0)[0])
-    pointArray.append(getPoint(100, 0, 50)[0])
-    pointArray.append(getPoint(100, 50, 50)[0])
-    pointArray.append(getPoint(100, 0, 0)[0])
-    pointArray.append(getPoint(100, 50, 0)[0])
+    pointArray.append(getPoint(originX, originY, originZ + Zsize)[0])
+    pointArray.append(getPoint(originX, originY + Ysize, originZ + Zsize)[0])
+    pointArray.append(getPoint(originX, originY, originZ)[0])
+    pointArray.append(getPoint(originX, originY + Ysize, originZ)[0])
+    pointArray.append(getPoint(originX + Xsize, originY, originZ + Zsize)[0])
+    pointArray.append(getPoint(originX + Xsize, originY + Ysize, originZ + Zsize)[0])
+    pointArray.append(getPoint(originX + Xsize, originY, originZ)[0])
+    pointArray.append(getPoint(originX + Xsize, originY + Ysize, originZ)[0])
 
-    faceArray = []
+    cubeFaces = []
 
-    faceArray.append(drawTriangle(pointArray[0], pointArray[3], pointArray[2], 0))
-    faceArray.append(drawTriangle(pointArray[0], pointArray[1], pointArray[3], 1))
-    faceArray.append(drawTriangle(pointArray[6], pointArray[5], pointArray[4], 2))
-    faceArray.append(drawTriangle(pointArray[6], pointArray[7], pointArray[5], 3))
-    faceArray.append(drawTriangle(pointArray[2], pointArray[4], pointArray[0], 4))
-    faceArray.append(drawTriangle(pointArray[2], pointArray[6], pointArray[4], 5))
-    faceArray.append(drawTriangle(pointArray[1], pointArray[5], pointArray[7], 6))
-    faceArray.append(drawTriangle(pointArray[1], pointArray[7], pointArray[3], 7))
-    faceArray.append(drawTriangle(pointArray[4], pointArray[5], pointArray[1], 8))
-    faceArray.append(drawTriangle(pointArray[4], pointArray[1], pointArray[0], 9))
-    faceArray.append(drawTriangle(pointArray[2], pointArray[7], pointArray[6], 10))
-    faceArray.append(drawTriangle(pointArray[2], pointArray[3], pointArray[7], 11))
+    cubeFaces.append(drawTriangle(pointArray[0], pointArray[3], pointArray[2], 0))
+    cubeFaces.append(drawTriangle(pointArray[0], pointArray[1], pointArray[3], 1))
+    cubeFaces.append(drawTriangle(pointArray[6], pointArray[5], pointArray[4], 2))
+    cubeFaces.append(drawTriangle(pointArray[6], pointArray[7], pointArray[5], 3))
+    cubeFaces.append(drawTriangle(pointArray[2], pointArray[4], pointArray[0], 4))
+    cubeFaces.append(drawTriangle(pointArray[2], pointArray[6], pointArray[4], 5))
+    cubeFaces.append(drawTriangle(pointArray[1], pointArray[5], pointArray[7], 6))
+    cubeFaces.append(drawTriangle(pointArray[1], pointArray[7], pointArray[3], 7))
+    cubeFaces.append(drawTriangle(pointArray[4], pointArray[5], pointArray[1], 8))
+    cubeFaces.append(drawTriangle(pointArray[4], pointArray[1], pointArray[0], 9))
+    cubeFaces.append(drawTriangle(pointArray[2], pointArray[7], pointArray[6], 10))
+    cubeFaces.append(drawTriangle(pointArray[2], pointArray[3], pointArray[7], 11))
 
-    def distanceKey(face):
-        return face[1]
-
-    faceArray.sort(key=distanceKey,reverse=True)
-
-    for face in faceArray:
-        if renderDebug.get("faces") == True or renderDebug.get("wireframe") == True: 
-            face[0].draw(window)
+    return cubeFaces
 
 
 def getPoint(x, y, z):
 
-    dx = x - player_x
-    dy = y - player_y
-    dz = z - player_z
+    dx = x - player.x
+    dy = y - player.y
+    dz = z - player.z
 
     dt = sqrt(dy**2 + dz**2)
     distance = sqrt(dt**2 + dx**2)
@@ -133,14 +155,14 @@ def getPoint(x, y, z):
         dirX = atan(dz)
         dirY = atan(dy)
 
-    dirX = dirX - player_dir_x
-    dirY = dirY - player_dir_y
+    dirX = dirX - player.dir.vertical
+    dirY = dirY - player.dir.horizontal
 
-    #pX = (dirX * window_width)/360
-    #pY = (dirY * window_height)/360
+    #pX = (dirX * window.width)/360
+    #pY = (dirY * window.height)/360
 
-    pX = dirX * (window.getWidth() * 360 / FOV) / 360 * 2*FOV
-    pY = dirY * (window.getHeight() * 360 / VFOV) / 360 * 2*VFOV
+    pX = dirX * (window.width * 360 / FOV.horizontal) / 360 * 2*FOV.horizontal
+    pY = dirY * (window.height * 360 / FOV.vertical) / 360 * 2*FOV.vertical
 
     point = Point(pX, pY)
 
@@ -169,120 +191,168 @@ def drawTriangle(point1, point2, point3, id):
 
     normalVector = getPoint(normalX+gravityCenterX, normalY+gravityCenterY, normalZ+gravityCenterZ)[0][0]
 
-    Vector = Line(gravityCenter, normalVector)
+    normalVector = Line(gravityCenter, normalVector)
 
     Triangle = Polygon(Vertices)
 
-    faceColor = colors[id]
-    faceColor = rgb(faceColor, faceColor, faceColor)
+    faceColor = color_index[id]
+    faceColor = rgb(faceColor[0],faceColor[1],faceColor[2])
 
     if renderDebug.get("faces") == True: 
         Triangle.setFill(faceColor)
     if renderDebug.get("faces") == True or renderDebug.get("wireframe") == True: 
-        #Triangle.draw(window)
         Triangle.setOutline(faceColor)
     if renderDebug.get("wireframe") == True: 
         Triangle.setOutline('black')
     if renderDebug.get("faceNormals") == True: 
-        Vector.setFill("orange")
-        Vector.draw(window)
+        normalVector.setFill("orange")
+        normalVector.draw(window)
         gravityCenter.draw(window)
 
     return (Triangle, distance)
 
 
 def clear():
-
+    # Undraws all faces for next frame
     for item in window.items[:]:
         item.undraw()
 
 
 def render():
 
+    # FPS clock
+    global FPS, last_clock_time
+
+    clock_time = time.time() - start_clock_time
+    FPS.timer = clock_time - last_clock_time
+    FPS.value += 1
+
+    # Clears window
     clear()
 
-    drawCube()
+    # Querry element faces to be sorted by distance and rendered
+    faceArray = []
+
+    faceArray.append(drawCube(50, 0, 0, 50, 50, 50))
+    faceArray.append(drawCube(50, 0, 50, 50, 50, 50))
+    faceArray.append(drawCube(50, 50, 50, 50, 50, 50))
+
+    faceArray = [currentFace for currentFaces in range(len(faceArray)) for currentFace in faceArray[currentFaces]]
+    print(str(faceArray[0])+"\n")
+
+    def distanceKey(face):
+        return face[1]
+
+    faceArray.sort(key=distanceKey,reverse=True)
+
+    for currentFace in faceArray:
+        if renderDebug.get("faces") == True or renderDebug.get("wireframe") == True: 
+            currentFace[0].draw(window)
+
+
+
+
 
     origin = Point(0, 0)
     origin = Circle(origin, 3)
     origin.setFill(color_rgb(255, 0, 0))
     origin.draw(window)
 
-    infos = str("(" + str(round(player_x, 3)) + ";" +
-                str(round(player_y, 3)) + ";" + str(round(player_z, 3)) + ")")
-    posDisplay = Text(Point(0, -window.getWidth()/4), infos)
+    if FPS.timer > 1:
+        FPS.lastValue = FPS.value
+        FPS.value = 0
+        last_clock_time = clock_time
+
+    FPS.counter = Text(Point(-window.width+50+len(str(FPS.lastValue))*9, window.height-25), "FPS:"+str(FPS.lastValue))
+    
+    if FPS.lastValue <= FPS.maxValue/3:
+        FPS.counter.setTextColor("red")
+    elif FPS.lastValue <= FPS.maxValue/1.5:
+        FPS.counter.setTextColor("orange")
+    else:
+        FPS.counter.setTextColor("green")
+
+    FPS.counter.draw(window)
+    infos = str("(" + str(round(player.x, 3)) + ";" +
+                str(round(player.y, 3)) + ";" + str(round(player.z, 3)) + ")")
+    posDisplay = Text(Point(0, -window.width/4), infos)
     posDisplay.draw(window)
 
-    infos = str("(" + str(player_dir_x) + ";" + str(player_dir_y) + ")")
-    dirDisplay = Text(Point(0, (-window.getWidth()/4)-30), infos)
+    infos = str("(" + str(player.dir.vertical) + ";" + str(player.dir.horizontal) + ")")
+    dirDisplay = Text(Point(0, (-window.width/4)-30), infos)
     dirDisplay.draw(window)
 
+
+
+
+# ------------ Render and keybinds -----------
 
 resetCoords()
 
 while True:
 
-    render()
-    update(165)
+    clock_time = time.time() - start_clock_time
 
+    render()
+    update(FPS.maxValue*1.1)
 
     '''if keyboard.is_pressed('s'):
 
-        player_x = player_x - (sin(player_dir_x))/360
-        player_z = player_z - (cos(player_dir_x))/360
+        player.x = player.x - (sin(player.dir.vertical))/360
+        player.z = player.z - (cos(player.dir.vertical))/360
 
     if keyboard.is_pressed('z'):
 
-        player_x = player_x + (sin(player_dir_x))
-        player_z = player_z + (cos(player_dir_x))
+        player.x = player.x + (sin(player.dir.vertical))
+        player.z = player.z + (cos(player.dir.vertical))
 
     if keyboard.is_pressed('d'):
 
-        player_dir_x += 1
+        player.dir.vertical += 1
 
     if keyboard.is_pressed('q'):
 
-        player_dir_x -= 1'''
+        player.dir.vertical -= 1'''
     
     if keyboard.is_pressed('s'):
 
-        player_x -= 1
+        player.x -= 1
 
     if keyboard.is_pressed('z'):
 
-        player_x += 1
+        player.x += 1
 
     if keyboard.is_pressed('space'):
 
-        player_y += 1
+        player.y += 1
 
     if keyboard.is_pressed('shift'):
 
-        player_y -= 1
+        player.y -= 1
 
     if keyboard.is_pressed('d'):
 
-        player_z += 1
+        player.z += 1
 
     if keyboard.is_pressed('q'):
 
-        player_z -= 1
+        player.z -= 1
 
     if keyboard.is_pressed('left_arrow'):
 
-        player_dir_x += 0.2
+        player.dir.vertical += 0.2
 
     if keyboard.is_pressed('right_arrow'):
 
-        player_dir_x -= 0.2
+        player.dir.vertical -= 0.2
 
     if keyboard.is_pressed('up_arrow'):
 
-        player_dir_y -= 0.2
+        player.dir.horizontal -= 0.2
 
     if keyboard.is_pressed('down_arrow'):
 
-        player_dir_y += 0.2
+        player.dir.horizontal += 0.2
 
     if keyboard.is_pressed('&'):
         if not(renderDebug.get("wireframe")):
@@ -308,13 +378,13 @@ while True:
         while keyboard.is_pressed('"'):
             sleep(0.1)
 
-    if player_dir_x < 0:
+    if player.dir.vertical < 0:
 
-        player_dir_x = player_dir_x + 360
+        player.dir.vertical = player.dir.vertical + 360
 
-    if player_dir_x > 360:
+    if player.dir.vertical > 360:
 
-        player_dir_x = player_dir_x - 360
+        player.dir.vertical = player.dir.vertical - 360
 
     if keyboard.is_pressed('esc'):
 
